bf41f4ba29b9785b1cfb4995899a230337b631b6
endepointe <endepointe@gmail.com>
2021-07-23 03:51:56 -0700

    connect the fkn api

--- Startup.cs
+++ Startup.cs
@@ -25,6 +25,7 @@ namespace EP
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHttpClient();
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
